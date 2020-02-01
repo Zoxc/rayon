33b4d7890ce0476f7fdb4a8da2b4575ba2b53cd2
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2020-02-01 23:00:09 +0100

    Give more general access to the thread-local value

--- rayon-core/src/tlv.rs
+++ rayon-core/src/tlv.rs
@@ -5,26 +5,17 @@ use std::cell::Cell;
 
 thread_local!(pub(crate) static TLV: Cell<usize> = Cell::new(0));
 
-/// Sets the current thread-local value to `value` inside the closure.
-/// The old value is restored when the closure ends
-pub fn with<F: FnOnce() -> R, R>(value: usize, f: F) -> R {
-    struct Reset(usize);
-    impl Drop for Reset {
-        fn drop(&mut self) {
-            TLV.with(|tlv| tlv.set(self.0));
-        }
-    }
-    let _reset = Reset(get());
-    TLV.with(|tlv| tlv.set(value));
-    f()
+/// Gives access to the thread-local value inside the closure.
+pub fn with<F: FnOnce(&Cell<usize>) -> R, R>(f: F) -> R {
+    TLV.with(f)
 }
 
 /// Sets the current thread-local value
-pub fn set(value: usize) {
+pub(crate) fn set(value: usize) {
     TLV.with(|tlv| tlv.set(value));
 }
 
 /// Returns the current thread-local value
-pub fn get() -> usize {
+pub(crate) fn get() -> usize {
     TLV.with(|tlv| tlv.get())
 }
