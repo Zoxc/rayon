898393705fab5070427d5d374afe60acbf68afc3
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2025-03-23 16:37:20 +0100

    Update tlv.rs
    fix

--- rayon-core/src/tlv.rs
+++ rayon-core/src/tlv.rs
@@ -3,28 +3,14 @@
 
 use std::cell::Cell;
 
-thread_local!(pub(crate) static TLV: Cell<usize> = Cell::new(0));
-
-/// Sets the current thread-local value to `value` inside the closure.
-/// The old value is restored when the closure ends
-pub fn with<F: FnOnce() -> R, R>(value: usize, f: F) -> R {
-    struct Reset(usize);
-    impl Drop for Reset {
-        fn drop(&mut self) {
-            TLV.with(|tlv| tlv.set(self.0));
-        }
-    }
-    let _reset = Reset(get());
-    TLV.with(|tlv| tlv.set(value));
-    f()
-}
+thread_local!(pub static TLV: Cell<*const ()> = Cell::new(std::ptr::null()));
 
 /// Sets the current thread-local value
 pub fn set(value: usize) {
-    TLV.with(|tlv| tlv.set(value));
+    TLV.with(|tlv| tlv.set(value as *const ()));
 }
 
 /// Returns the current thread-local value
 pub fn get() -> usize {
-    TLV.with(|tlv| tlv.get())
+    TLV.with(|tlv| tlv.get()as *const ()) as usize
 }
